cd538ad52bed70b06e432337b1845866444031bd
aroranipun12 <aroranipun1@gmail.com>
2022-01-11 22:37:02 +0530

    Login forms

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const Login = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userData = {
+      username,
+      password,
+    };
+    setUser(userData);
+    setUserName("");
+    setPassword("");
+  };
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2> Login</h2>
+      <form
+        style={{
+          display: "grid",
+          alignItems: "center",
+          justifyItems: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(event) => setUserName(event.target.value)}
+          value={username}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+          value={password}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {user && JSON.stringify(user, null, 2)}
+    </div>
+  );
+};
+
+export default Login;
